cac7427f29b5670c7f24bfd65ce1a93db1fe4202
oloruntobi <awoderuoloruntobi@gmail.com>
2019-10-03 15:09:25 +0100

    Added like button

---
+++ vidly/src/components/counters.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+class Counters extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        
+    }
+}
+ 
+export default Counters;
