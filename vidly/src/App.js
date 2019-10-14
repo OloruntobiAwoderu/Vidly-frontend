06c9f76a9ad723cdc3936c3ca09c2e8588db0045
oloruntobi <awoderuoloruntobi@gmail.com>
2019-10-14 13:16:35 +0100

    Implemented routing

--- vidly/src/App.js
+++ vidly/src/App.js
@@ -1,18 +1,25 @@
-import React, { Component } from 'react';
-import Movies from './components/movies'
+import React, { Component } from "react";
+import { Route, Redirect, Switch } from "react-router-dom";
+import Movies from "./components/movies";
+import Customers from "./components/customers";
+import Rentals from "./components/rentals";
+import NotFound from "./components/notFound";
 
 class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {  }
-    }
-    render() { 
-        return ( 
-            <main className="container">
-                <Movies/>
-            </main>
-         );
-    }
+  render() {
+    return (
+      <main className="container">
+        <Switch>
+          <Route path="/movies" component={Movies} />
+          <Route path="/customers" component={Customers} />
+          <Route path="/rentals" component={Rentals} />
+          <Route path="/not-found" component={NotFound} />
+          <Redirect from="/" exact to="/movies" />
+          <Redirect to="/not-found" />
+        </Switch>
+      </main>
+    );
+  }
 }
- 
+
 export default App;
