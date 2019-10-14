c8ad80251f6a6a2b392b16ae1529c618092f3170
oloruntobi <awoderuoloruntobi@gmail.com>
2019-10-14 13:02:22 +0100

    Created three new components

---
+++ vidly/src/components/movieForm.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+const MovieForm = () => {
+  return (
+    <div>
+      <h1>Movie Form</h1>
+      <button className="primary">save</button>
+    </div>
+  );
+};
+
+export default MovieForm;
