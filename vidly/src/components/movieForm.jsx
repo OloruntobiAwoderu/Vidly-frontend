42768fc283513d002963b22a2dba71c12699c90a
oloruntobi <awoderuoloruntobi@gmail.com>
2019-10-14 17:35:04 +0100

    Added login form

--- vidly/src/components/movieForm.jsx
+++ vidly/src/components/movieForm.jsx
@@ -7,7 +7,7 @@ const MovieForm = ({ match, history}) => {
       <button className="btn btn-primary" onClick={() => history.push('/movies')}>save</button>
     </div>
   );
-};
+}; 
 
 export default MovieForm;
  
