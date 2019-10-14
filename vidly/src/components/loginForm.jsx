df25b5b067ca1e9d1a7629f2af5cf391d54e3ad5
oloruntobi <awoderuoloruntobi@gmail.com>
2019-10-14 18:37:34 +0100

    Refactored Login Form

--- vidly/src/components/loginForm.jsx
+++ vidly/src/components/loginForm.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-
+import Input from "./common/input";
 class LoginForm extends Component {
   state = {
     account: { username: "", password: "" }
@@ -8,40 +8,30 @@ class LoginForm extends Component {
   handleSubmit = e => {
     e.preventDefault();
   };
-  handleChange = ({ currentTarget: input })=> {
+  handleChange = ({ currentTarget: input }) => {
     const account = { ...this.state.account };
     account[input.name] = input.value;
     this.setState({ account });
   };
   render() {
-      const { account } = this.state;
+    const { account } = this.state;
     return (
       <div>
         <h1>Login</h1>
         <form onSubmit={this.handleSubmit}>
-          <div className="form-group">
-            <label htmlFor="username">Username</label>
-            <input
-              value={account.username}
-              onChange={this.handleChange}
-              autoFocus
-              id="username"
-              name="username"
-              type="text"
-              className="form-control"
-            />
-          </div>
-          <div className="form-group">
-            <label htmlFor="password">Password</label>
-            <input
-              value={account.password}
-              onChange={this.handleChange}
-              name="password"
-              id="password"
-              type="text"
-              className="form-control"
-            />
-          </div>
+          <Input
+            name="username"
+            value={account.username}
+            label="Username"
+            onChange={this.handleChange}
+          />
+          <Input
+            name="password"
+            value={account.password}
+            label="Password"
+            onChange={this.handleChange}
+          />
+          
           <button className="btn btn-primary">Login</button>
         </form>
       </div>
