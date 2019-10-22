d91376f749fd3f5056128f1b791f91aaffe3cbf7
oloruntobi <awoderuoloruntobi@gmail.com>
2019-10-22 12:55:18 +0100

    Set Token to local storage

--- vidly/src/components/loginForm.jsx
+++ vidly/src/components/loginForm.jsx
@@ -1,8 +1,7 @@
 import React, { Component } from "react";
 import joi from "joi-browser";
 import Form from "./common/form";
-import {  login } from '../services/authService'
-
+import { login } from "../services/authService";
 
 class LoginForm extends Form {
   state = {
@@ -20,34 +19,29 @@ class LoginForm extends Form {
       .required()
       .label("Password")
   };
-  
 
-    doSubmit = async ( ) => {
-      try {
-        const { data } = this.state
-        await login(data.username, data.password)
-      }
-        
-       catch (ex) {
-        if(ex.response && ex.response.status === 400){
-          const errors = { ...this.state.errors};
-          errors.username = ex.response.data;
-          this.setState({ errors });
-        }
+  doSubmit = async () => {
+    try {
+      const { data } = this.state;
+    const { data: jwt } = await login(data.username, data.password);
+    localStorage.setItem('token', jwt)
+    } catch (ex) {
+      if (ex.response && ex.response.status === 400) {
+        const errors = { ...this.state.errors };
+        errors.username = ex.response.data;
+        this.setState({ errors });
       }
     }
-  
-  
+  };
+
   render() {
-   
     return (
       <div>
         <h1>Login</h1>
         <form onSubmit={this.handleSubmit}>
-         {this.renderInput('username', 'Username')}
-         {this.renderInput('password', 'Password', 'password')}
-          {this.renderButton('Login')}
-          
+          {this.renderInput("username", "Username")}
+          {this.renderInput("password", "Password", "password")}
+          {this.renderButton("Login")}
         </form>
       </div>
     );
