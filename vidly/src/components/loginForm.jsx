c0560b796edc68ac8ee22ad4f2730e9cc889aa72
oloruntobi <awoderuoloruntobi@gmail.com>
2019-10-22 13:09:09 +0100

    Redirected registration form to home page

--- vidly/src/components/loginForm.jsx
+++ vidly/src/components/loginForm.jsx
@@ -22,9 +22,10 @@ class LoginForm extends Form {
 
   doSubmit = async () => {
     try {
-      const { data } = this.state;
+    const { data } = this.state;
     const { data: jwt } = await login(data.username, data.password);
     localStorage.setItem('token', jwt)
+    this.props.history.push('/');
     } catch (ex) {
       if (ex.response && ex.response.status === 400) {
         const errors = { ...this.state.errors };
